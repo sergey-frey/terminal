5366c364b585cdea41e4d4d733b06730e89100da
sergey-frey <sergey.frey.work@gmail.com>
2025-06-01 12:59:07 +0300

    fix: add error message for echo command wtout arguments

--- src/terminal/lib/command-handlers/echo.ts
+++ src/terminal/lib/command-handlers/echo.ts
@@ -1,7 +1,17 @@
 import type { TerminalContent } from "../../types/terminal-content";
+import { TERMINAL_MESSAGES } from "../terminal-messages";
 
 export const getEchoCommandOutput = (command: string): TerminalContent[] => {
   const args = command.split(" ").slice(1);
+
+  if (args.length === 0) {
+    return [
+      {
+        type: "text-command-error",
+        error: TERMINAL_MESSAGES.NO_ARGUMENTS_PROVIDED,
+      },
+    ];
+  }
 
   return args.map<TerminalContent>((arg) => ({
     type: "text-command-output",
